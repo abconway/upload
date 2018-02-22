69ce87826e42f8dd7568365bc4f0b92cbf3a358f
Alex Conway <abconway2@gmail.com>
2018-02-21 22:27:20 -0500

    Remove the second address line
    - It wasn't needed

--- customer/models.py
+++ customer/models.py
@@ -6,7 +6,6 @@ from localflavor.us.models import USStateField, USZipCodeField, STATE_CHOICES
 class Customer(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
-    street_address_line_1 = models.CharField(max_length=256)
-    street_address_line_2 = models.CharField(max_length=256)
+    street_address_line = models.CharField(max_length=256)
     state = USStateField(max_length=2, choices=STATE_CHOICES)
     zip_code = USZipCodeField()
